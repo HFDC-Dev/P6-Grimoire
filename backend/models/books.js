7808924b5dc9a31c9a1607c13035702ef4ced2ca
HFDC-Dev <henriquedacosta.pro@gmail.com>
2025-08-19 17:11:29 +0200

    Création du dossier models et ajout du modèle books.js

---
+++ backend/models/books.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const bookSchema = mongoose.Schema({
+    userId: { type: String, required: true },
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    year: { type: Number, required: true },
+    genre: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    averageRating: { type: Number, required: true, min: 0, max: 5 }
+});
+
+module.exports = mongoose.model('Book', bookSchema);
