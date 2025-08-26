7f5e5aa0a02f537544072cc4a197254dfecd8b88
HFDC-Dev <henriquedacosta.pro@gmail.com>
2025-08-26 19:41:05 +0200

    Ajout du dossier controllers + déplacement de la logique des routes books vers controllers/books.js

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,32 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const Book = require('../models/books');
+const bookCtrl = require('../controllers/books')
 
 // Ajouter un livre
-router.post('/', (req, res, next) => {
-    delete req.body._id;
-    const book = new Book({
-        ...req.body
-    });
-    book.save()
-        .then(() => res.status(201).json({ message: 'Livre enregistré !' }))
-        .catch(error => res.status(400).json({ error }));
-});
+router.post('/', bookCtrl.createBook);
 
 // Récupérer tous les livres
-router.get('/', (req, res, next) => {
-    Book.find()
-        .then(books => res.status(200).json(books))
-        .catch(error => res.status(400).json({ error }));
-});
+router.get('/', bookCtrl.getAllBooks);
 
 // Récupérer un seul livre par ID
-router.get('/:id', (req, res, next) => {
-    Book.findOne({ _id: req.params.id })
-        .then(book => res.status(200).json(book))
-        .catch(error => res.status(404).json({ error }));
-});
+router.get('/:id', bookCtrl.getOneBook);
 
 
 module.exports = router;
