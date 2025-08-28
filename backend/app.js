b1e2d59e4534f53f9e0064c9ef154749c72c76c8
HFDC-Dev <henriquedacosta.pro@gmail.com>
2025-08-29 00:43:09 +0200

    Ajout du fichier user.js avec infrastructure prête dans le dossier controllers et ajout du fichier user.js dans le dossier routes + enregistrement du router dans app.js

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const bookRoutes = require('./routes/books')
+const bookRoutes = require('./routes/books');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -22,7 +23,8 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/api/books', bookRoutes)
+app.use('/api/books', bookRoutes);
+app.use('/api/auth', userRoutes);
 
 
 module.exports = app;
