e4384167211702f92245fd9444f23618f7ea21a7
HFDC-Dev <henriquedacosta.pro@gmail.com>
2025-08-29 18:35:26 +0200

    Création du dossier middleware et du fichier auth.js pour l’authentification et ajout de ce middleware sur les routes book pour sécuriser l’accès

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,16 +1,19 @@
 const express = require('express');
+const auth = require('../middleware/auth');
 const router = express.Router();
+
 
 const bookCtrl = require('../controllers/books')
 
 // Ajouter un livre
-router.post('/', bookCtrl.createBook);
+router.post('/', auth, bookCtrl.createBook);
 
 // Récupérer tous les livres
 router.get('/', bookCtrl.getAllBooks);
 
 // Récupérer un seul livre par ID
-router.get('/:id', bookCtrl.getOneBook);
+router.get('/:id', auth, bookCtrl.getOneBook);
+
 
 
 module.exports = router;
